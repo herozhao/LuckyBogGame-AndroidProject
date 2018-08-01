759b4e00599ab3cc650a33d7221314f841bea77b
niulongjie@ren001.com <niulongjie@ren001.com>
2018-08-01 10:40:12 +0800

    抓娃娃安卓工程

---
+++ app/src/main/java/jhz/lockyboygame/MainActivity.java
@@ -0,0 +1,13 @@
+package jhz.lockyboygame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
